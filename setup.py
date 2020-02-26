d1f4b29d641e6690df868f10e42b3ecf9cf81378
sam <san@sam-xps-13-9360.byod.gmu.edu>
2020-02-26 16:10:51 -0500

    added setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='ImpossibleArcade',
+      version='0.0.1',
+      description='Simple Arcade with AI that are (impossible) to beat [CS321 Semester Project]',
+      url='http://github.com/SamuelSchmidgall/ImpossibleArcade',
+      author='Samuel Schmidgall, ',
+      author_email='sschmidg@gmu.edu',
+      license='MIT',
+      packages=['impossible_arcade'],
+      install_requires=[
+          "numpy",
+          "pygame",
+      ],
+      zip_safe=False)
